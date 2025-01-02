0014864dabc0233bf87b07a92e2f227b2f9eed0d
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-02 17:24:40 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+
 #!/bin/bash
+cd ~/
 echo "not fully automatic"
 echo "Updating & Upgrading Your System"
 sudo apt update && sudo apt upgrade -y
@@ -6,11 +8,15 @@ echo "Installing required packages"
 sudo apt install -y lubuntu-desktop lxqt fluxbox i3 awesomewm websockify novnc 
 echo "manually setup your vncserver configuration"
 vncserver
-cd ~/
 git clone https://github.com/NoVNC/NoVNC
+echo "You need create a new self-signed certificate inside the noVNC folder so that you can connect over HTTPS"
+cd noVNC
+openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem
 echo "speeding up NoVnc Speed using numpy"
 sudo apt-get install -y python3-pip
 pip3 install numpy
+echo -e "moving the xstartup file from novnc-lxqt to the .vnc folder in /home/"
+cp -r novnc-lxqt/xstartup
 echo "making start.sh executable"
 chmod u+x start.sh
 echo "Runing the server"
