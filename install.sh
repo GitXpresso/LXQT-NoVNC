4a3dd38f1ef4ce5d5927fb2d923babfbf1237bdc
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-02 17:04:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ echo "Installing required packages"
 sudo apt install -y lubuntu-desktop lxqt fluxbox i3 awesomewm
 cd ~/
 git clone https://github.com/NoVNC/NoVNC
+cd noVNC
+openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem
 echo "making start.sh executable"
 chmod u+x start.sh
 echo "Runing the server"
