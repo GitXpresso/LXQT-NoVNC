5f0d2b45adb31a0f410ef803fb5ca5178a782a97
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-02 17:18:39 -0500

    Update install.sh
    Added pip3 and numpy installation

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@ echo "not fully automatic"
 echo "Updating & Upgrading Your System"
 sudo apt update && sudo apt upgrade -y
 echo "Installing required packages"
-sudo apt install -y lubuntu-desktop lxqt fluxbox i3 awesomewm
+sudo apt install -y lubuntu-desktop lxqt fluxbox i3 awesomewm websockify novnc 
+echo "manually setup your vncserver configuration"
+vncserver
 cd ~/
 git clone https://github.com/NoVNC/NoVNC
-cd noVNC
-openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem
+echo "speeding up NoVnc Speed using numpy"
+sudo apt-get install -y python3-pip
+pip3 install numpy
 echo "making start.sh executable"
 chmod u+x start.sh
 echo "Runing the server"
