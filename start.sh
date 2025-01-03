f3a4d646dbd830d7a6f147bb73d2069310ee8a98
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-02 22:42:36 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+websockify -D \
+    --web /usr/share/novnc/ \
+    8443 \
+    localhost:5901
 IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "Connect with https://$IP:8443/vnc.html?host=$IP&port=8443"
 vncserver :1
