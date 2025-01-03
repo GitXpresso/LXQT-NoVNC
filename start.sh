29baf4978f19afabe4f9696b54230ad83b86eb3a
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-02 22:37:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "Connect with https://$IP:8443/vnc.html?host=$IP&port=8443"
 vncserver :1
-~/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 8443
+~/NoVNC/utils/novnc_proxy --vnc localhost:5901 --listen 8443
 vncserver -kill :1
