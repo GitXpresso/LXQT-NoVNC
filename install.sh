8d0b4bc5a7e5d101b5cbe594eb234f3ce3338648
William gw <gitmocha@outlook.com>
2025-01-09 13:31:31 +0000

    added BBlack for echo command

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 # Script made to make a little bit faster for installing lxqt novnc
 BWhite='\033[1;37m'       # White
 BYellow='\033[1;33m'      # Yellow
+BBlack='\033[1;30m'        # Black
 cd ~/ 
 git clone https://github.com/gitxpresso/Linux-NOVNC
 sudo apt update
-echo "installing tasksel"
+echo -e "${BBlack}installing tasksel"
 sudo apt install tasksel -y
-echo "install dialog for tasksel to work"
+echo -e "${BBlack}install dialog for tasksel to work"
 sudo apt install dialog -y
-echo "Installing tightvncserver"
+echo -e "${BBlack}Installing tightvncserver"
 sudo apt install tigervnc-standalone-server tigervnc-viewer -y
 echo "make sure your terminal is tall and wide, or just tall in order for dialog to work"
-echo "${BWhite}if you get an apt-get error with tasksel"
-echo "${BYellow}starting tasksel"
+echo -e "${BWhite}if you get an apt-get error with tasksel"
+echo -e "${BYellow}starting tasksel"
 sudo tasksel
