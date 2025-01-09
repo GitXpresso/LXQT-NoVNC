438a6ede1925ad07b3af2411086672cdf28f030e
William gw <gitmocha@outlook.com>
2025-01-09 13:22:08 +0000

    added tasksel and dialog installiation in bash file install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Script made to make a little bit faster for installing lxqt novn
+cd ~/ 
+git clone https://github.com/gitxpresso/Linux-NOVNC
 sudo apt update
-echo "install the default window manager for lxqt"
-sudo apt install openbox -y
+echo "installing tasksel"
+sudo apt install tasksel -y
+echo "install dialog for tasksel to work"
+sudo apt install dialog -y
 echo "installing the LXQT Package"
 sudo apt install lxqt -y
 echo "Installing tightvncserver"
-sudo apt install tightvncserver -y
+sudo apt install tigervnc-standalone-server tigervnc-viewer -y
