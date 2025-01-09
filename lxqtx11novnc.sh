8451b5c8da351117629329a924146c78da62e251
William gw <gitmocha@outlook.com>
2025-01-09 20:52:46 +0000

    added correct link to linksandcredits.txt

--- lxqtx11novnc.sh
+++ lxqtx11novnc.sh
@@ -3,7 +3,7 @@ sudo apt update
 echo "installing lxqt novnc xfvb openbix websockify dialog tigervnc-viewer tigervnc-standalone-server"
 sudo apt install lxqt x11vnc xvfb openbox websockify novnc dialog tigervnc-standalone-server tigervnc-viewer -y
 echo "starting the server"
-ssh -L 5900:localhost:5900 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/startlxqt -nopw -listen 127.0.0.1 -forever"
 vncserver -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -localhost no :0
 websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6080 localhost:5900
+ssh -L 5900:localhost:5900 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/startlxqt -nopw -listen 127.0.0.1 -forever"
 echo "Vnc Server Started"
