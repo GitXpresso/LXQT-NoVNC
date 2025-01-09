258b7906da6b1712f0ebf5aa8685d96e1b1f8d7a
William gw <gitmocha@outlook.com>
2025-01-09 18:52:36 +0000

    updated newly added script

--- lxqtx11novnc.sh
+++ lxqtx11novnc.sh
@@ -3,7 +3,7 @@ sudo apt update
 echo "installing lxqt novnc xfvb openbix websockify dialog tigervnc-viewer tigervnc-standalone-server"
 sudo apt install lxqt x11vnc xvfb openbox websockify novnc dialog tigervnc-standalone-server tigervnc-viewer -y
 echo "starting the server"
-ssh -L 5900:localhost:5900 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/startlxqt -nopw -listen 127.0.0.1 -forever"
-vncserver -SecurityTypes none -localhost no :5
-websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6890 localhost:5900
+ssh -L 5908:localhost:5908 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/mate-session -nopw -listen 127.0.0.1 -forever"
+vncserver -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -localhost no :8
+websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6887 localhost:5908
 echo "Vnc Server Started"
