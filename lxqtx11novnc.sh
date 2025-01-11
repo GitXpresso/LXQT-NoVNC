3824092d162efd669af6bc5043720da1b902e054
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-10 20:13:58 -0500

    Update lxqtx11novnc.sh
    added the correct directory

--- lxqtx11novnc.sh
+++ lxqtx11novnc.sh
@@ -4,6 +4,6 @@ echo "installing lxqt novnc xfvb openbix websockify dialog tigervnc-viewer tiger
 sudo apt install lxqt x11vnc xvfb openbox websockify novnc dialog tigervnc-standalone-server tigervnc-viewer -y
 echo "starting the server"
 vncserver -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -localhost no :0
-websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6080 localhost:5900
+websockify -D --web=/usr/share/novnc/ --cert=/home/linux-novnc/novnc.pem 6080 localhost:5900
 ssh -L 5900:localhost:5900 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/startlxqt -nopw -listen 127.0.0.1 -forever"
 echo "Vnc Server Started"
