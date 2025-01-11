43a3236ea93c5c0ec89fc11b8ac76c570bbec4a8
William <126926699+GitXpresso@users.noreply.github.com>
2025-01-10 20:50:25 -0500

    Update lxqtx11novnc.sh
    added colors and colors to the echo command and added "#!/bin/bash" because it feels empty without it

--- lxqtx11novnc.sh
+++ lxqtx11novnc.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
+BBlack='\033[1;30m'       # Bolded Black 
+No_Color='\033[0m'        # Default Color
+BYellow='\033[1;33m'      # Bolded Yellow
+
+
 echo "cloning gitxpresso/linux-novnc in home directory in order https certificate work properly for the websockify server"
 cd ~/
 git clone https://github.com/gitxpresso/linux-novnc
-echo "Updating Your System"
+echo "${BBlack}Updating Your System"
 sudo apt update
-echo "installing lxqt novnc xfvb openbix websockify dialog tigervnc-viewer tigervnc-standalone-server"
+echo "${BBlack}installing lxqt novnc xfvb openbix websockify dialog tigervnc-viewer tigervnc-standalone-server"
 sudo apt install lxqt x11vnc xvfb openbox websockify novnc dialog tigervnc-standalone-server tigervnc-viewer -y
 echo "starting the server"
 vncserver -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE  -localhost no :0
 websockify -D --web=/usr/share/novnc/ --cert=/home/linux-novnc/novnc.pem 6080 localhost:5900
 ssh -L 5900:localhost:5900 192.168.86.5 "x11vnc -create -env FD_PROG=/usr/bin/startlxqt -nopw -listen 127.0.0.1 -forever"
 echo "Vnc Server Started"
+echo "Go To ${BYellow}https://localhost:6080${No_Color}In your browser to access the VNC Server"
